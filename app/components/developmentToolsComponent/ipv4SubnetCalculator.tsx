5d46b4889814e06a0de4ca74cc840db38c84e216
omkar hole <omkarhole314@gmail.com>
2026-03-03 14:55:08 +0530

    fixed

--- app/components/developmentToolsComponent/ipv4SubnetCalculator.tsx
+++ app/components/developmentToolsComponent/ipv4SubnetCalculator.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 interface SubnetResults {
   networkAddress: string;
